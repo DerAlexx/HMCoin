e760d32845f0361d2bf9f14f4cc1c3b3d9704724
DerAlexx <alexweb199@hotmail.de>
2021-02-11 14:51:49 +0100

    (Added) Propof-of-Work parts

--- hmcoin-frontend/js-frontend/src/services/mining.js
+++ hmcoin-frontend/js-frontend/src/services/mining.js
@@ -4,6 +4,8 @@ import React from "react";
 import Axios from "axios";
 import axios from 'axios';
 
+import sha256 from 'crypto-js/sha256';
+
 import Menubar from '../component/menubar';
 
 export default class Mining extends React.Component {
@@ -11,54 +13,73 @@ export default class Mining extends React.Component {
   BLOCKCHAINAPI = "http://0.0.0.0:8000/rest/mining/"
 
   state = {
-    pof: [],
+    miningtrans: "",
+    miningtrans_original: "",
+    number: 0,
+    tohash: undefined
   }
 
   constructor(props) {
     super(props);
-    this.interval = 0;
+    this.number = 0;
+    this.work = this.work.bind(this);
   }
 
-  componentWillMount() {
-    try {
-      this.interval = setInterval(async() => {
-        Axios.get(this.BLOCKCHAINAPI).then(Response => {
-          const chuncks = Response.data
-          console.log(chuncks)
-          this.setState({
-            pof: chuncks,
-          })
-        }).catch(errors => {
-          this.setState({
-            pof: [],
-          })
-        })
-      }, 1000)
-    } catch (exception) {
-      console.log(exception)
-    }
+  componentDidMount() {
+    Axios.get(this.BLOCKCHAINAPI).then(Response => {
+      const chuncks = Response.data
+      this.setState({
+        miningtrans: chuncks,
+      })
+      this.work(chuncks)
+    }).catch(errors => {
+      this.setState({
+        miningtrans: "",
+      })
+    })
+    
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
+  //miningtrans.id + num + miningtrans.sender
+  work(miningtrans) {
+    var tohash = miningtrans.id + miningtrans.proof + miningtrans.sender
+    var hash = sha256(tohash)
+    var number_of_runs = 0
+    do {
+      number_of_runs++
+    } while(!hash.startsWith("11"))
   }
 
-    render() {
-      const {pof} = this.state
+  render() {
+      const {miningtrans, number, tohash} = this.state
       return (       
           <div className="App">
               <Menubar></Menubar>
               <div style={{marginLeft: '10%', width:'80%', top: '120px', position: 'absolute', zIndex:-20}}>
                 <h2 style={{color:'gray'}}>Mining</h2>
                 <hr></hr>
-                <h3 style={{color:'#ff5a5a'}} className="float-left">Mining</h3>
-                <br></br>
-                <br></br>
-                <ul class="list-group">
                 {
-                  pof.id
+                  miningtrans < 1 ? 
+                  <div class="alert alert-warning">
+                    No open transactions for mining
+                  </div> 
+                  :
+                  <div class="card">
+                    <div class="card-body">
+                      #{miningtrans.id}, Sender: {miningtrans.sender}, Recipient: {miningtrans.recipient}, Quantity: {miningtrans.quantity}, Reward: {miningtrans.reward}
+                    </div>
+                  </div>
                 }
-                </ul>
+                <hr></hr>
+                1) Getting a secret: {number}
+                <br></br>
+                <br></br>
+                2) The value to hash: {tohash}
+                <br></br>
+                <br></br>
+                3) The hash:
+                <hr></hr>
+                <button>Send hash</button>
               </div>
           </div>
       )
